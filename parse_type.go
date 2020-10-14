3e1f8d7d3b85033d9933ac5c3bcef47ec17ced4c
dalianzhu <yinzihao1@126.com>
2020-10-14 11:29:22 +0800

    feat

--- parse_type.go
+++ parse_type.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "log"
-    "regexp"
-    "strings"
+	"log"
+	"regexp"
+	"strings"
 )
 
 func ParseType(text string) []*NeedCommentLine {
-    re, err := regexp.Compile(`var\s+([A-Z][a-zA-Z0-9]*)\s+[a-zA-Z0-9*=]+|const\s+([A-Z][a-zA-Z0-9]*)\s+\=`)
-    if err != nil {
-        log.Println(err)
-        return []*NeedCommentLine{}
-    }
-    lines := strings.Split(text, "\n")
-    willComments := filterRegex(re, lines)
-    return willComments
+	re, err := regexp.Compile(`var\s+([A-Z][a-zA-Z0-9]*)\s+[a-zA-Z0-9*=]+|type\s+([A-Z][a-zA-Z0-9]*)\s+[a-zA-Z0-9*=()]+|const\s+([A-Z][a-zA-Z0-9]*)\s+\=`)
+	if err != nil {
+		log.Println(err)
+		return []*NeedCommentLine{}
+	}
+	lines := strings.Split(text, "\n")
+	willComments := filterRegex(re, lines)
+	return willComments
 }
