145ba4fe33d09751838008a59b2f3f4004bff852
dalianzhu <yinzihao1@126.com>
2020-12-16 13:35:45 +0800

    fix:

--- parse_func.go
+++ parse_func.go
@@ -1,67 +1,66 @@
 package main
 
 import (
-    "log"
-    "regexp"
-    "strings"
+	"log"
+	"regexp"
+	"strings"
 )
 
 // ParseFunc ...
 func ParseFunc(text string) []*NeedCommentLine {
-    // func (r *ResenderMsg) IsSendCtx() context.Context {
-    re, err := regexp.Compile(`^func\s+([A-Z][a-zA-Z0-9_-]+)\(|^func\s*\([a-zA-Z0-9]+\s+[*a-zA-Z0-9]+\)\s+([A-Z][a-zA-Z0-9_-]+)`)
-    if err != nil {
-        log.Println(err)
-        return []*NeedCommentLine{}
-    }
-    lines := strings.Split(text, "\n")
-    willComments := make([]*NeedCommentLine, 0)
-    for i, line := range lines {
-        ret := re.FindAllStringSubmatch(line, -1)
-        if len(ret) >= 1 {
-            matchArr := ret[0]
-            if len(matchArr) >= 3 {
-                var funcName string
-                if matchArr[1] != "" {
-                    funcName = matchArr[1]
-                } else {
-                    funcName = matchArr[2]
-                }
+	// func (r *ResenderMsg) IsSendCtx() context.Context {
+	re, err := regexp.Compile(`^func\s+([A-Z][a-zA-Z0-9_-]+)\(|^func\s*\([a-zA-Z0-9]+\s+[*a-zA-Z0-9]+\)\s+([A-Z][a-zA-Z0-9_-]+)`)
+	if err != nil {
+		log.Println(err)
+		return []*NeedCommentLine{}
+	}
+	lines := strings.Split(text, "\n")
+	willComments := make([]*NeedCommentLine, 0)
+	for i, line := range lines {
+		ret := re.FindAllStringSubmatch(line, -1)
+		if len(ret) >= 1 {
+			matchArr := ret[0]
+			if len(matchArr) >= 3 {
+				var funcName string
+				if matchArr[1] != "" {
+					funcName = matchArr[1]
+				} else {
+					funcName = matchArr[2]
+				}
 
-                log.Printf("func name:%v\n", funcName)
-                tp := new(NeedCommentLine)
-                tp.Name = funcName
-                tp.OriginLineNo = i
-                willComments = append(willComments, tp)
-            }
-        }
-    }
-    return willComments
+				log.Printf("func name:%v\n", funcName)
+				tp := new(NeedCommentLine)
+				tp.Name = funcName
+				tp.OriginLineNo = i
+				willComments = append(willComments, tp)
+			}
+		}
+	}
+	return willComments
 }
 
 func filterRegex(re *regexp.Regexp, lines []string) []*NeedCommentLine {
-    willComments := make([]*NeedCommentLine, 0)
-    for i, line := range lines {
-        ret := re.FindAllStringSubmatch(line, -1)
-        if len(ret) >= 1 {
-            matchArr := ret[0]
-            if len(matchArr) >= 2 {
-                var funcName string
-                if matchArr[1] != "" {
-                    funcName = matchArr[1]
-                } else {
-                    if len(matchArr) >= 3 {
-                        funcName = matchArr[2]
-                    }
-                }
+	willComments := make([]*NeedCommentLine, 0)
+	for i, line := range lines {
+		ret := re.FindAllStringSubmatch(line, -1)
+		if len(ret) >= 1 {
+			matchArr := ret[0]
+			var funcName string
+			if len(matchArr) >= 2 {
+				for _, v := range matchArr[1:] {
+					if strings.Trim(v, "\t\r\n ") != "" {
+						funcName = v
+						break
+					}
+				}
 
-                log.Printf("func name:%v\n", funcName)
-                tp := new(NeedCommentLine)
-                tp.Name = funcName
-                tp.OriginLineNo = i
-                willComments = append(willComments, tp)
-            }
-        }
-    }
-    return willComments
+				log.Printf("func name:%v\n", funcName)
+				tp := new(NeedCommentLine)
+				tp.Name = funcName
+				tp.OriginLineNo = i
+				willComments = append(willComments, tp)
+			}
+		}
+	}
+	return willComments
 }
--- parse_func_test.go
+++ parse_func_test.go
@@ -1,174 +1,37 @@
 package main
 
 import (
-    "log"
-    "testing"
+	"log"
+	"testing"
 )
 
 // TestParseFunc ...
 func TestParseFunc(t *testing.T) {
 
-    text := `package controller
+	text := `package controller
 import (
     "context"
-    log "github.com/sirupsen/logrus"
-    "reflect"
-    "sync"
-    "sync/atomic"
-    "time"
 )
 
-// 一个滑动窗口的resender
 
-// NewResendMsg ...
 func NewResendMsg(ctx context.Context, uuid string, timestamp int64, data interface{}) *ResenderMsg {
-    r := &ResenderMsg{}
-    r.Uuid = uuid
-    r.Timestamp = timestamp
-    r.Data = data
-    r.isSend, r.isSendCancel = context.WithCancel(ctx)
-    return r
 }
 
-// 重新发送未确认的msg
+func newResendMsg(ctx context.Context, uuid string, timestamp int64, data interface{}) *ResenderMsg {
+}
+
 type ResenderMsg struct {
-    Data         interface{}
-    Timestamp    int64
-    SendTimes    int
-    isSend       context.Context
-    isSendCancel context.CancelFunc
-    Uuid         string
 }
 
 func (r *ResenderMsg) isSendCtx() context.Context {
-    return r.isSend
 }
 
-// IsSendCancel ...
 func (r *ResenderMsg) IsSendCancel() {
-    r.isSendCancel()
 }
-
-// NewMsgResender ...
-func NewMsgResender(ctx context.Context, subId int, sender func(data *ResenderMsg) error) *MsgResender {
-    s := new(MsgResender)
-    s.ctx = ctx
-    s.subId = subId
-    s.queue = make(chan *ResenderMsg, 50)
-    s.dataMap = sync.Map{}
-    s.unsendCount = 0
-    s.sender = sender
-    return s
-}
-
-// MsgResender ...
-// MsgResender ...
-type MsgResender struct {
-    subId       int
-    dataMap     sync.Map
-    unsendCount int64
-    sender      func(data *ResenderMsg) error
-    queue       chan *ResenderMsg
-    ctx         context.Context
-}
-
-// UnSendCount ...
-func (m *MsgResender) UnSendCount() int64 {
-    return m.unsendCount
-}
-
-// Wait ...
-func (m *MsgResender) Wait() {
-    for m.unsendCount != 0 {
-        time.Sleep(time.Second)
-    }
-}
-
-// Put ...
-func (m *MsgResender) Put(key string, data *ResenderMsg) {
-    data.Uuid = key
-    atomic.AddInt64(&m.unsendCount, 1)
-    m.dataMap.Store(key, data)
-    select {
-    case <-m.ctx.Done():
-    case m.queue <- data:
-    }
-
-}
-
-// Pop ...
-func (m *MsgResender) Pop(f func(key string, data *ResenderMsg) bool) bool {
-    var ret bool
-    select {
-    case <-m.ctx.Done():
-    case d := <-m.queue:
-        ret = f(d.Uuid, d)
-        m.dataMap.Delete(d.Uuid)
-    }
-    return ret
-}
-
-// IsSend ...
-func (m *MsgResender) IsSend(key string) {
-    v, ok := m.dataMap.Load(key)
-    if !ok {
-        log.Infof("MsgResender data not found:%v", key)
-        return
-    }
-
-    msg, ok := v.(*ResenderMsg)
-    if !ok {
-        log.Errorf("MsgResender data type error:%v", reflect.TypeOf(v))
-        return
-    }
-    msg.IsSendCancel()
-    atomic.AddInt64(&m.unsendCount, -1)
-}
-
-// Maintenance ...
-func (m *MsgResender) Maintenance() {
-    m.dataMap.Range(func(key, value interface{}) bool {
-        data, ok := value.(*ResenderMsg)
-        if !ok {
-            log.Errorf("MsgResender Maintenance type error:%v, subId:%v", reflect.TypeOf(value), m.subId)
-        }
-
-        select {
-        case <-data.IsSendCtx().Done():
-            // 已经发送成功的消息跳过去
-            return true
-        case <-m.ctx.Done():
-            return false
-        default:
-            if time.Now().Unix()-data.Timestamp > 10 {
-                data.SendTimes++
-                err := m.sender(data)
-                if err != nil {
-                    log.Errorf("MsgResender Maintenance send msg error:%v, subId:%v", err, m.subId)
-                    return false
-                }
-            }
-            return true
-        }
-    })
-}`
-
-    text = `
-// 一个滑动窗口的resender
-
-// NewResendMsg ...
-func NewResendMsg(ctx context.Context, uuid string, timestamp int64, data interface{}) *ResenderMsg {
-    r := &ResenderMsg{}
-    r.Uuid = uuid
-    r.Timestamp = timestamp
-    r.Data = data
-    r.isSend, r.isSendCancel = context.WithCancel(ctx)
-    return r
-}
-
 `
-    willComments := ParseFunc(text)
-    for _,item:=range willComments{
-        log.Printf("line:%v, funcName:=%v\n", item.OriginLineNo, item.Name)
-    }
+
+	willComments := ParseFunc(text)
+	for _, item := range willComments {
+		log.Printf("line:%v, funcName:=%v\n", item.OriginLineNo, item.Name)
+	}
 }
